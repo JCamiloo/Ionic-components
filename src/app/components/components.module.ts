4852dbe2c3b291ecd164675bf0d6e407e08b528e
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-06-27 23:52:38 -0500

    Menu started

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { PopinfoComponent } from './popinfo/popinfo.component';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    MenuComponent,
     PopinfoComponent
   ],
   exports: [
     HeaderComponent,
+    MenuComponent,
     PopinfoComponent
   ],
   imports: [

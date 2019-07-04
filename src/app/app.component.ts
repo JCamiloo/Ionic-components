8f94d09bf807b102c82b85c42f35fce1ed3c9dc4
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-07-03 22:01:15 -0500

    Tabs working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
+import { DataService } from 'src/app/services/data.service';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Componente } from './app.interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  componentes:Observable<Componente[]>
+ 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private data: DataService
   ) {
     this.initializeApp();
   }
@@ -21,6 +27,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.componentes = this.data.getMenuOptions();
     });
   }
 }
